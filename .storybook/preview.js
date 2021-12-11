a0ac0a00d0cac3dceeac092b19ebb88ac4600c2e
Elías Rodríguez Rivas <eliasrdrgz@gmail.com>
2021-12-11 13:35:37 +0100

    added a11y addon

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 
-import { GlobalStyle } from '../src/shared/global';
+import { GlobalStyle } from "../src/shared/global";
 
 /*
  * Global decorator to apply the styles to all stories
@@ -8,7 +8,7 @@ import { GlobalStyle } from '../src/shared/global';
  * https://storybook.js.org/docs/react/writing-stories/decorators#global-decorators
  */
 export const decorators = [
-  Story => (
+  (Story) => (
     <>
       <GlobalStyle />
       <Story />
@@ -17,9 +17,16 @@ export const decorators = [
 ];
 
 /*
- * Read more about global parameters at:
+ * More on Storybook global parameters at:
  * https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
  */
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  // Storybook a11y addon configuration
+  a11y: {
+    // the target DOM element
+    element: "#root",
+    // sets the execution mode for the addon
+    manual: false,
+  },
 };
